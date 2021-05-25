a846399e61ad79902dccc70253a6db709d1cc0d6
alonsovz <fabiomejiash@gmail.com>
2021-05-25 15:12:15 -0600

    execution time

--- BACKEND/actFijo/public/index.php
+++ BACKEND/actFijo/public/index.php
@@ -12,6 +12,7 @@ header("Access-Control-Max-Age: 1000");
 header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
 header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
 
+ini_set('max_execution_time', 300);
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
